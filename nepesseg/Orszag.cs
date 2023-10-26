2a96cc49c4af6397b3bca37be368fe56c1786791
C20ÁrpásK <C20rpsK@203-02>
2023-10-26 09:31:53 +0200

    Add project files.

---
+++ nepesseg/Orszag.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nepesseg
+{
+    class Orszag
+    {
+        public string Orszagnev { get; private set; }
+        public int Terulet { get; private set; }
+        public int Nepesseg { get; private set; }
+        public string Fovaros { get; private set; }
+        public int FovarosNepesseg { get; private set; }
+        public bool FovarosbanKoncentralt => FovarosNepesseg * 1000 >= Nepesseg * 0.3;
+
+        public Orszag(string sor)
+        {
+            var o = sor.Split(';');
+            Orszagnev = o[0];
+            Terulet = int.Parse(o[1]);
+            Nepesseg = int.Parse(o[2].Replace("g", "0000"));
+            Fovaros = o[3];
+            FovarosNepesseg = int.Parse(o[4]);
+        }
+
+        public int Nepsuruseg => Nepesseg / Terulet;
+
+        public override string ToString()
+        {
+            return "";
+        }
+    }
+
+}
---
+++ nepesseg/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Text;
+
+namespace nepesseg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var orszagok = new List<Orszag>();
+
+            using StreamReader sr = new(@"..\..\..\src\adatok-utf8.txt", Encoding.UTF8);
+
+            _ = sr.ReadLine();
+
+            while (!sr.EndOfStream)
+            {
+                orszagok.Add(new(sr.ReadLine()));
+            }
+
+            Console.WriteLine("4. feladat");
+            Console.WriteLine($"A beolvasott országok száma {orszagok.Count}.\n");
+
+            Console.WriteLine("5. feladat");
+
+            Orszag kina = orszagok.Single(o => o.Orszagnev == "Kína");
+
+            Console.WriteLine($"Kína népsűrűsége {kina.Nepsuruseg} fő/km^2.");
+
+            Console.WriteLine("6. feladat");
+
+            Orszag india = orszagok.Single(o => o.Orszagnev == "India");
+
+            int nepessegKulonbseg = kina.Nepesseg - india.Nepesseg;
+
+            Console.WriteLine($"Kínában a lakosság {nepessegKulonbseg} fővel volt több.");
+
+            Console.WriteLine("7. feladat");
+
+            var harmadikLegnepesebb = orszagok.OrderByDescending(o => o.Nepesseg).ElementAt(2);
+
+            Console.WriteLine($"A harmadik legnépesebb ország: {harmadikLegnepesebb.Orszagnev}, a lakosság {harmadikLegnepesebb.Nepesseg} fő");
+
+            Console.WriteLine("8. feladat - A következő országok lakosságának több mint 30%-a a fővárosban lakik:");
+
+            var fovarosbanKoncentraltak = orszagok.Where(o => o.FovarosbanKoncentralt).ToList();
+
+            foreach (var f in fovarosbanKoncentraltak)
+            {
+                Console.WriteLine($"\t{f.Orszagnev} ({f.Fovaros})");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
